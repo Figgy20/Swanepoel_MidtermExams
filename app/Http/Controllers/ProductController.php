dc62db7727316bc5f380d1261ee7dd5745a883fb
Aurony Taloban <ajimatchi@gmail.com>
2024-06-20 16:59:08 +0800

    Added Controller for Product List

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function getProducts()
+    {   
+        $products = array(
+            'product1' => 'Steelheart',
+            'product2' => 'Hunger Games',
+            'product3' => 'Hunger Games: Catching Fire',
+            'product4' => 'Hunger Games: Mockingjay'
+        );
+
+        return view('user', ['products' => $products]);
+    }
+}
