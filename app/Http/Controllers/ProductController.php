2bf8f4003eb56fa258eb869ee21571bbe92a09b7
Figgy20 <keaganswanepoel10d@gmail.com>
2024-06-20 21:38:08 +0800

    Adjusted the use statements

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,6 +16,8 @@ class ProductController extends Controller
             'product4' => 'Hunger Games: Mockingjay'
         );
 
+
+
         return view('welcome', ['products' => $products]);
     }
 }
